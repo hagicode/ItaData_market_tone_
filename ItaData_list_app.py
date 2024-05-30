0915963f694447b3d61474a0ef62546b54ed875c
hagicode <131940602+hagicode@users.noreply.github.com>
2024-05-30 11:04:10 +0900

    Add files via upload

--- ItaData_list_app.py
+++ ItaData_list_app.py
@@ -36,7 +36,7 @@ with st.expander("じぶんの銘柄リストから絞込む"):
     st.markdown('<p style="font-family:sans-serif; color:blue; font-size: 10px;">活用例：四季報・株探などファンダで絞込んだリスト／自分の取引銘柄など</p>', unsafe_allow_html=True)
     uploaded_file = st.file_uploader("マイリストアップロード", type='csv') 
     if uploaded_file is not None:
-        upload_df = pd.read_csv(uploaded_file,index_col=None)
+        upload_df = pd.read_csv(uploaded_file,index_col=None,header=None)
         mycode_lists_org = upload_df.iloc[:,0]
         mycode_lists = [int(s) for s in mycode_lists_org if is_int(s)]
         mylist_button = st.radio("マイリストでの絞込み",    ('無', '有'), horizontal=True)
